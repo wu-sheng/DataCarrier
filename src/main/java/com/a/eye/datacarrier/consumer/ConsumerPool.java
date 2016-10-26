e1b1beb91b5565f2257aff3b350df6f4bd0a001a
wusheng <wu.sheng@foxmail.com>
2016-10-26 15:08:04 +0800

    使用一个新的锁

--- src/main/java/com/a/eye/datacarrier/consumer/ConsumerPool.java
+++ src/main/java/com/a/eye/datacarrier/consumer/ConsumerPool.java
@@ -4,6 +4,7 @@ import com.a.eye.datacarrier.buffer.Buffer;
 import com.a.eye.datacarrier.buffer.Channels;
 
 import java.util.ArrayList;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Pool of consumers
@@ -11,10 +12,10 @@ import java.util.ArrayList;
  * Created by wusheng on 2016/10/25.
  */
 public class ConsumerPool<T> {
-    private Object           lock;
     private boolean          running;
     private ConsumerThread[] consumerThreads;
     private Channels<T>      channels;
+    private ReentrantLock    lock;
 
     public ConsumerPool(Channels<T> channels, IConsumer<T> prototype, int num, boolean usePrototypeCopies) {
         running = false;
@@ -23,7 +24,7 @@ public class ConsumerPool<T> {
         for (int i = 0; i < num; i++) {
             consumerThreads[i] = new ConsumerThread("DataCarrier.Consumser." + i + ".Thread", usePrototypeCopies ? getNewConsumerInstance(prototype) : prototype);
         }
-        lock = new Object();
+        lock = new ReentrantLock();
     }
 
     private IConsumer<T> getNewConsumerInstance(IConsumer<T> prototype) {
@@ -40,16 +41,15 @@ public class ConsumerPool<T> {
         if (running) {
             return;
         }
-        synchronized (lock) {
-            if (!running) {
-                synchronized (lock) {
-                    this.allocateBuffer2Thread();
-                    for (ConsumerThread consumerThread : consumerThreads) {
-                        consumerThread.start();
-                    }
-                    running = true;
-                }
+        try {
+            lock.lock();
+            this.allocateBuffer2Thread();
+            for (ConsumerThread consumerThread : consumerThreads) {
+                consumerThread.start();
             }
+            running = true;
+        } finally {
+            lock.unlock();
         }
     }
 
@@ -63,7 +63,7 @@ public class ConsumerPool<T> {
             ArrayList<Integer>[] threadAllocation = new ArrayList[channelSize];
             for (int threadIndex = 0; threadIndex < consumerThreads.length; threadIndex++) {
                 int index = threadIndex % channelSize;
-                if(threadAllocation[index] == null){
+                if (threadAllocation[index] == null) {
                     threadAllocation[index] = new ArrayList<Integer>();
                 }
                 threadAllocation[index].add(threadIndex);
@@ -77,11 +77,11 @@ public class ConsumerPool<T> {
                 for (int i = 0; i < threadAllocationPerChannel.size(); i++) {
                     int threadIndex = threadAllocationPerChannel.get(i);
                     int start = i * step;
-                    int end = i == threadAllocationPerChannel.size()-1? bufferSize : (i + 1) * step;
+                    int end = i == threadAllocationPerChannel.size() - 1 ? bufferSize : (i + 1) * step;
                     consumerThreads[threadIndex].addDataSource(channel, start, end);
                 }
             }
-        }else {
+        } else {
             /**
              * if consumerThreads.length < channelSize
              * each consumer will process several channels.
@@ -89,7 +89,7 @@ public class ConsumerPool<T> {
              * if consumerThreads.length == channelSize
              * each consumer will process one channel.
              */
-            for(int channelIndex = 0; channelIndex < channelSize; channelIndex++){
+            for (int channelIndex = 0; channelIndex < channelSize; channelIndex++) {
                 int consumerIndex = channelIndex % consumerThreads.length;
                 consumerThreads[consumerIndex].addDataSource(channels.getBuffer(channelIndex));
             }
@@ -98,14 +98,13 @@ public class ConsumerPool<T> {
     }
 
     public void close() {
-        synchronized (lock) {
-            if (running) {
-                synchronized (lock) {
-                    for (ConsumerThread consumerThread : consumerThreads) {
-                        consumerThread.shutdown();
-                    }
-                }
+        try {
+            lock.lock();
+            for (ConsumerThread consumerThread : consumerThreads) {
+                consumerThread.shutdown();
             }
+        } finally {
+            lock.unlock();
         }
     }
 }
